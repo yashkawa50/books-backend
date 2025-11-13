1a2a501382d8fc3dbf0abfa1264dfe27189c5cfe
Yash <yashkawa82@gmail.com>
2025-11-13 16:01:52 +0530

    cors domain

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ["https://books-frontend-pink.vercel.app"], // your frontend domain
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
